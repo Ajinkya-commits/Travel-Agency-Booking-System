9f6f1f3f0070329672e12fd0635fc9537a29e182
Ajinkya-commits <ajinkyathoke8585@gmail.com>
2024-12-17 02:09:09 +0530

    updated mongo connection

--- Backend/server.js
+++ Backend/server.js
@@ -5,21 +5,21 @@ import cors from "cors";
 import bookingRoute from "./routes/bookingRoutes.js";
 import adminRouter from "./routes/adminRoute.js";
 import packageRouter from "./routes/packageRoutes.js";
-import dotenv from "dotenv";
+
 const app = express();
-dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://frontend-9vicbgw76-ajinkya-commits-projects.vercel.app",
     methods: ["POST", "GET", "PUT", "DELETE"],
   })
 );
+
 app.use(express.json());
 
 mongoose
-  .connect(process.env.Mongo_Url)
+  .connect(process.env.MONGO_URL)
   .then(() => console.log("connected to mongoDb"))
   .catch((e) => console.error("error connecting to db", e));
 
